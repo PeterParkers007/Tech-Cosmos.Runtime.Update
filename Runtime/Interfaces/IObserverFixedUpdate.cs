df79e0d852167717e4413743cc8c24dadc53e9aa
PeterParkers007 <3427463164@qq.com>
2025-10-19 22:31:39 +0800

    关于Update的优化

---
+++ Runtime/Interfaces/IObserverFixedUpdate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ZJM_Optimize.Runtime.Interfaces
+{
+    public interface IObserverFixedUpdate
+    {
+        public void ObserverFixedUpdate();
+    }
+}
+
